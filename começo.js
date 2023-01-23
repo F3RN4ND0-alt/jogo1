2e0579b07ae70e15e5a63803881675d7742b4b5c
Fernando Soethe <fernando.nando.fsk@gmail.com>
2023-01-23 09:42:22 -0300

    commit inicial

---
+++ começo.js
@@ -0,0 +1,23 @@
+var paragrafo = document.querySelector('p','h1')
+var cursor = document.querySelector('.cursor')
+var texto = paragrafo.innerHTML
+var index = 0
+
+const escrever = () => {
+  paragrafo.innerHTML = paragrafo.innerHTML.replace('|', '')
+
+  if (texto.length > index) {
+    if (index == 0){
+      paragrafo.innerHTML = texto.charAt(index) 
+    } else {
+      paragrafo.innerHTML += texto.charAt(index) 
+    }
+
+    paragrafo.innerHTML += '|'
+
+    index++
+    setTimeout(escrever, 50)
+
+  } 
+}
+escrever()
